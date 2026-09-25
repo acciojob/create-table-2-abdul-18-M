35f2ee597082047552aad59881accf323b268cdf
Abdul Mulla <abdulmulla8071@gmail.com>
2026-09-25 17:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+const tables = document.querySelector("#myTable");
+
 function createTable() {
     //Write your code here
+
+	let rn = prompt("Input number of rows")
+	let cn = prompt("Input number of column")
+
+	if(rn === null || cn === null || isNaN(rn) || isNaN(cn)){
+		return
+	}
+	rn = Number(rn)
+	cn = Number(cn)
+
+	if(rn <=0||cn<=0){
+		alert("Please enter postivie number")
+		return
+	}
+
+	for(let i = 0;i<rn;i++){
+		const rows = document.createElement("tr")
+
+		for(let j = 0;j<cn;j++){
+			const cells = document.createElement("td")
+
+			cells.innerHTML = `Row-${i} Column-${j}`
+			rows.appendChild(cells)
+		}
+		tables.appendChild(rows)
+	}
+
+
+	
+	
   
 }
